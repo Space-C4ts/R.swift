8cc1d51befd607b1efce3b9267f94257db426510
Space-C4ts <72996875+Space-C4ts@users.noreply.github.com>
2023-09-28 18:17:30 +0700

    - Update script

--- Plugins/RswiftGenerateInternalResources/RswiftGenerateInternalResources.swift
+++ Plugins/RswiftGenerateInternalResources/RswiftGenerateInternalResources.swift
@@ -51,34 +51,37 @@ import XcodeProjectPlugin
 extension RswiftGenerateInternalResources: XcodeBuildToolPlugin {
     func createBuildCommands(context: XcodePluginContext, target: XcodeTarget) throws -> [Command] {
 
-        let resourcesDirectoryPath = context.pluginWorkDirectory
-            .appending(subpath: target.displayName)
-            .appending(subpath: "Resources")
+      guard let target = target as? SourceModuleTarget else { return [] }
 
-        try FileManager.default.createDirectory(atPath: resourcesDirectoryPath.string, withIntermediateDirectories: true)
+      let outputDirectoryPath = context.pluginWorkDirectory
+          .appending(subpath: target.name)
 
-        let rswiftPath = resourcesDirectoryPath.appending(subpath: "R.generated.swift")
+      try FileManager.default.createDirectory(atPath: outputDirectoryPath.string, withIntermediateDirectories: true)
 
-        let description: String
-        if let product = target.product {
-            description = "\(product.kind) \(target.displayName)"
-        } else {
-            description = target.displayName
-        }
+      let rswiftPath = outputDirectoryPath.appending(subpath: "R.generated.swift")
 
-        return [
-            .buildCommand(
-                displayName: "R.swift generate resources for \(description)",
-                executable: try context.tool(named: "rswift").path,
-                arguments: [
-                    "generate", rswiftPath.string,
-                    "--target", target.displayName,
-                    "--input-type", "xcodeproj",
-                    "--bundle-source", "finder",
-                ],
-                outputFiles: [rswiftPath]
-            ),
-        ]
+      let sourceFiles = target.sourceFiles
+          .filter { $0.type == .resource || $0.type == .unknown }
+          .map(\.path.string)
+
+      let inputFilesArguments = sourceFiles
+          .flatMap { ["--input-files", $0 ] }
+
+      let bundleSource = target.kind == .generic ? "module" : "finder"
+      let description = "\(target.kind) module \(target.name)"
+
+      return [
+          .buildCommand(
+              displayName: "R.swift generate resources for \(description)",
+              executable: try context.tool(named: "rswift").path,
+              arguments: [
+                  "generate", rswiftPath.string,
+                  "--input-type", "input-files",
+                  "--bundle-source", bundleSource,
+              ] + inputFilesArguments,
+              outputFiles: [rswiftPath]
+          ),
+      ]
     }
 }
 
